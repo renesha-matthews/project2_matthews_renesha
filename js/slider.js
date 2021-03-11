2ade1ef0ea325cc587422264f64a8c4785ac6a62
Renesha Matthews <reneshamatthews9@gmail.com>
2021-03-11 10:56:46 -0500

    removed slick, added bxslider

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,5 @@
-/** https://kenwheeler.github.io/slick/ **/
+//https://bxslider.com/install/
 
-$('.slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
-});
+$(document).ready(function(){
+    $('.slider').bxSlider();
+  });
